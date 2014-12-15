27a3d2895bae973c7e596cd3a253a6b65712bf7b
sasapet <sasapet@yahoo.com>
2014-12-14 23:05:46 -0500

    chapter 1

---
+++ test/hello_spec.js
@@ -0,0 +1,5 @@
+﻿describe("Hello", function () {
+    it("says hello", function () {
+        expect(sayHello('Jane')).toBe("Hello, Jane!");
+    });
+});
---
+++ test/scope_spec.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="../Scripts/jasmine.js" />
+/* jshint globalstrict: true */
+/* global Scope: false */
+
+'use strict';
+describe("Scoope", function () {
+    it("can be constructed and used as an objec", function () {
+        var scope = new Scope();
+        scope.aProperty = 1;
+
+        expect(scope.aProperty).toBe(1);
+    });
+
+    describe("digest", function () {
+        var scope;
+        beforeEach(function () {
+            scope = new Scope();
+        });
+        it("calls the listener function of a watch on first $digest", function () {
+            var watchFn = function () { return 'wat'; };
+            var listenerFn = jasmine.createSpy();
+            scope.$watch(watchFn, listenerFn);
+            scope.$digest();
+
+            expect(listenerFn).toHaveBeenCalled();
+        });
+        it("calls the watch function with the scope as the arument", function () {
+            var watchFn = jasmine.createSpy();
+            var listenerFn = function () { };
+            scope.$watch(watchFn, listenerFn);
+            scope.$digest();
+
+            expect(watchFn).toHaveBeenCalledWith(scope);
+        });
+    });
+});
