5655796b0139c7a3cc0d4f376c9e77c907432d7c
sasapet <sasapet@yahoo.com>
2015-01-22 21:53:02 -0500

    provider 1

--- src/loader.js
+++ src/loader.js
@@ -1,5 +1,52 @@
 ﻿/* jshint globalstrict: true */
 'use strict';
+
 function setupModuleLoader(window) {
-    var angular = (window.angular = window.angular || {});
+  var ensure = function(obj, name, factory) {
+    return obj[name] || (obj[name] = factory());
+  };
+
+  var angular = ensure(window, 'angular', Object);
+
+  var createModule = function(name, requires, modules) {
+    if (name === 'hasOwnProperty') {
+      throw 'hasOwnProperty is not a valid module name';
+    }
+    var invokeQueue = [];
+    var invokeLater = function (method) {
+        return function () {
+            invokeQueue.push([method, arguments]);
+            return moduleInstance;
+        };
+    };
+    var moduleInstance = {
+      name: name,
+      requires: requires,
+      constant: invokeLater('constant'),
+      provider: invokeLater('provider'),
+      _invokeQueue: invokeQueue
+    };
+    modules[name] = moduleInstance;
+    return moduleInstance;
+  };
+
+  var getModule = function(name, modules) {
+    if (modules.hasOwnProperty(name)) {
+      return modules[name];
+    } else {
+      throw 'Module '+name+' is not available!';
+    }
+  };
+
+  ensure(angular, 'module', function() {
+    var modules = {};
+    return function(name, requires) {
+      if (requires) {
+        return createModule(name, requires, modules);
+      } else {
+        return getModule(name, modules);
+      }
+    };
+  });
+
 }
